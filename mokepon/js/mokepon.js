6375fd79f10d7d9bbc104cba0eb544e26ddbf175
Marcos Veron <marcosveronar@gmail.com>
2022-08-05 15:20:25 -0300

    Parte de elección de personajes terminada

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -7,27 +7,45 @@ function iniciarJuego(){
 
 }
 
-
-
-
-function seleccionarMascotaJugador() {
-    let elegirHipodoge = document.getElementById('hipodoge')
-    let elegirCapipepo = document.getElementById('capipepo')
-    let elegirRatigueya = document.getElementById('ratigueya')
-
-    if(elegirHipodoge.checked) {
-        alert("Seleccionaste a Hipodoge 💧💧💧")
-    }
-    if(elegirCapipepo.checked) {
-        alert("Seleccionaste a Capipepo 🌳🌳🌳")
-    }
-    if(elegirRatigueya.checked) {
-        alert("Seleccionaste a Ratigueya 🔥🔥🔥")
-    }
-
+function aleatorio(min,max) {
+    return Math.floor(Math.random() * (max-min +1) + min)
 }
 
 
+// Seleccionamos a traves del input radio nuestra mascota
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascotaJugador')
+    
+
+    if(inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML = "Hipodoge"
+    }
+    else if(inputCapipepo.checked) {
+        spanMascotaJugador.innerHTML = "Capipepo"
+    }
+    else if(inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = "Ratigueya"
+    }
+    else {
+        alert("Debe seleccionarse una mascota.")
+    }
+    // Cuando ya elegimos nuestra mascota, procederemos a usar la función que nombra a la mascota rival de forma aleatoria
+
+    seleccionarMascotaEnemigo()
+    
+}
+
+// Seleccionamos de forma al azar el nombre de la mascota enemiga
+function seleccionarMascotaEnemigo() {
+    let spanMascotaEnemigo = document.getElementById('mascotaEnemigo')
+    let mascotaEnemigo = ['Hipodoge','Capipepo','Ratigueya']
+    let enemigo = mascotaEnemigo[aleatorio(0,2)]
+    
+    spanMascotaEnemigo = spanMascotaEnemigo.innerHTML = `${enemigo}`
+}
 
 // Carga los elementos cuando el html termina de renderizarse
 window.addEventListener('load', iniciarJuego)
